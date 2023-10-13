67ea10573684a65c5d9f03aeba1cb311292ed5b5
piklz <pixelpiklz@gmail.com>
2023-10-13 20:44:04 +0100

    t

--- presto_launch.sh
+++ presto_launch.sh
@@ -216,11 +216,11 @@ echo -e "\033[1;37m       looking for presto Git updates\e[0m"
 #--- eg:  git fetch origin main (or develop might add this as arg for user)
 
 #gets remote repo changes of branch otherwise local repo has no way of knowiung if its diff and so git status command will incrreclty show up to date
-if is_command git ]  ; then
-  git fetch
+if [ is_command git ]; then
+ git fetch
 else
  # Git is not installed, install it now
-  sudo apt get install git
+ sudo apt get install git
 fi
 
 if [ $(git status | grep -c "Your branch is up to date") -eq 1 ]; then
