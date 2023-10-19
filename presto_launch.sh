23646b6cdd8f430582d1df02a8ce7a31517778c9
piklz <pixelpiklz@gmail.com>
2023-10-19 14:04:39 +0100

    q

--- presto_launch.sh
+++ presto_launch.sh
@@ -23,7 +23,7 @@
 ##################################################################################################
 #lets gooooooooo
 
-presto_VERSION='1.0.6' #lol3
+presto_VERSION='1.0.7' #monkey
 
 
 INTERACTIVE=True
