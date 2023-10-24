f0ac8a6ca79256edbb8646945a654ba558923416
piklz <pixelpiklz@gmail.com>
2023-10-24 18:22:01 +0100

    q

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Check if the WireGuard interface is up
-
 if ip link show eth0 &> /dev/null ; then
     exit 0 # The interface is up, so the container is healthy
     echo -e "The interface is up, so the container is healthy"
