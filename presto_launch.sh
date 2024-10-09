a75a8ae87b440fa7cce2468a2740acd188ae8d88
piklz <nomonkeynocircus@gmail.com>
2024-10-09 17:52:59 +0100

    Update presto_launch.sh

--- presto_launch.sh
+++ presto_launch.sh
@@ -384,14 +384,15 @@ function yml_builder() {
 	#if an env file exists check for timezone
 	[ -f "./services/$1/$1.env" ] && timezones ./services/$1/$1.env
  
+ 	echo "" >>docker-compose.yml
  	 # Append the service only if it's not already in the docker-compose.yml file
 	if ! grep -q "services:$1:" docker-compose.yml; then
 	  cat $service >>docker-compose.yml
 	fi
 
 	#add new line then append service
-	echo "" >>docker-compose.yml
-	cat $service >>docker-compose.yml
+	#echo "" >>docker-compose.yml
+	#cat $service >>docker-compose.yml
   
 }
 
