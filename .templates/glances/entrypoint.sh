a1ac1353988cc1295fa3d3e6c867ec9f3eefdd09
piklz <pixelpiklz@gmail.com>
2025-08-12 11:42:06 +0100

    e

--- .templates/glances/entrypoint.sh
+++ .templates/glances/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+# The template is mounted from your host to this path
 TEMPLATE_FILE="/app/glances.conf.template"
-FINAL_CONFIG_FILE="/app/glances.conf"
 
-if [ ! -f "$FINAL_CONFIG_FILE" ]; then
-    echo "Glances config not found. Copying template."
-    cp "$TEMPLATE_FILE" "$FINAL_CONFIG_FILE"
+# The final config file is in the mounted volume
+FINAL_CONFIG_FILE="/glances/conf/glances.conf"
+
+# Only overwrite the config file if it's empty.
+# This prevents overwriting user changes on a restart.
+if [ ! -s "$FINAL_CONFIG_FILE" ]; then
+    echo "Glances config is empty. Creating from template."
+    envsubst < "$TEMPLATE_FILE" > "$FINAL_CONFIG_FILE"
 fi
 
 exec glances -C "$FINAL_CONFIG_FILE" -w
