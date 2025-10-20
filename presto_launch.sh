d28257f4e0625d6a61d826a0505304cab673ab9e
piklz <pixelpiklz@gmail.com>
2025-10-20 18:11:42 +0100

    adding initial MQTT support (homeassistant and others ) and mosquitto added to the list of apps

--- presto_launch.sh
+++ presto_launch.sh
@@ -738,12 +738,13 @@ declare -A cont_array=(
     [ittools]="it-tools > nice IT. tools in one place "
     [immich]="Immich > your own google photos+ai tagging (need more ram 4gb+ recommended)"
     [prestox728]="presto-x728 > presto's UPS Hat monitor(geekworms-x728_v1.2) *hardware only"
+    [mosquitto]="Mosquitto > lightweight MQTT broker for IoT messaging for prestox728"
 )
 
 declare -a aarch64_keys=(
     "portainer" "sonarr" "radarr" "lidarr" "jackett" "qbittorrent" "jellyfin" "plex" "tautulli" "overseerr"
     "heimdall" "homeassistant" "motioneye" "homarr" "wireguard" "pihole" "wgui" "uptimekuma" "syncthing"
-    "photoprism" "glances" "prowlarr" "homepage" "ittools" "immich" "prestox728"
+    "photoprism" "glances" "prowlarr" "homepage" "ittools" "immich" "prestox728" "mosquitto"
 )
 
 yml_builder() {
